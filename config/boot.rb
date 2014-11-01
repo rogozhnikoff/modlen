4110d5501947bb602b47470aea07d51c172c5842
Murad Rogozhnikov <murad@rogozhnikoff.com>
2014-11-01 18:24:24 +0200

    custom.js to coffee

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# change default port
+require 'rails/commands/server'
+module Rails
+  class Server
+    alias :default_options_alias :default_options
+    def default_options
+      default_options_alias.merge!(:Port => 8080)
+    end
+  end
+end
